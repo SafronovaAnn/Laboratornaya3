07eb1e0362d7a7dcc12590593c09595504699af5
SafronovaAnn <dolgovaannochka05@mail.ru>
2026-05-03 15:54:32 +0700

    Лабораторная работа №3

--- go_series.go
+++ go_series.go
@@ -1,56 +1,117 @@
 package main
- 
+
 import (
-    "fmt"
-    "math"
+	"fmt"
 )
- 
+
+// Функция для нахождения НОД (алгоритм Евклида)
+func gcd(x, y int64) int64 {
+	for y != 0 {
+		x, y = y, x%y
+	}
+	return x
+}
+
+// Функция для сокращения дроби
+func reduceFraction(numerator, denominator *int64) {
+	g := gcd(abs(*numerator), abs(*denominator))
+	*numerator /= g
+	*denominator /= g
+}
+
+func abs(x int64) int64 {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
 func main() {
-    var a, b int
-    var eps float64
- 
-    fmt.Println("СУММА БЕСКОНЕЧНОГО РЯДА")
- 
-    fmt.Print("\nВведите а (от 1 до 10): ")
-    fmt.Scan(&a)
-    fmt.Print("Введите b (от 1 до 10): ")
-    fmt.Scan(&b)
-    fmt.Print("Введите точность eps: ")
-    fmt.Scan(&eps)
- 
-    // Проверка ввода
-    if a < 1 || a > 10 || b < 1 || b > 10 {
-        fmt.Println("Ошибка! а и b должны быть от 1 до 10")
-        return
-    }
- 
-    // Если b = 1, ряд расходится
-    if b == 1 {
-        fmt.Println("\nРезультат: infinity")
-        return
-    }
- 
-    sum := 0.0
-    var term float64
-    n := 1
- 
-    for {
-        // Вычисляем n^a и b^n с помощью math.Pow
-        term = math.Pow(float64(n), float64(a)) / math.Pow(float64(b), float64(n))
-        sum += term
-        n++
- 
-        // Защита от бесконечного цикла
-        if n > 1000 {
-            break
-        }
- 
-        // Выходим, когда член стал меньше точности
-        if term < eps {
-            break
-        }
-    }
- 
-    fmt.Printf("\nСумма ряда: %.3f\n", sum)
-    fmt.Printf("Количество итераций: %d\n", n-1)
+	var a, b int
+	fmt.Println("Сумма бесконечного ряда")
+
+	fmt.Print("\nВведите a: ")
+	fmt.Scan(&a)
+	fmt.Print("Введите b: ")
+	fmt.Scan(&b)
+
+	if a < 1 || a > 10 || b < 1 || b > 10 {
+		fmt.Println("Ошибка! a и b должны быть от 1 до 10")
+		return
+	}
+
+	// Если b = 1, ряд расходится
+	if b == 1 {
+		fmt.Println("\nРезультат: infinity")
+		return
+	}
+
+	var numerator int64 = 0
+	var denominator int64 = 1
+
+	// Вычисляем числитель и знаменатель по формулам
+	switch a {
+	case 1:
+		numerator = int64(b)
+		denominator = int64((b - 1) * (b - 1))
+	case 2:
+		numerator = int64(b * (b + 1))
+		denominator = int64((b - 1) * (b - 1) * (b - 1))
+	case 3:
+		numerator = int64(b * (b*b + 4*b + 1))
+		denominator = int64((b - 1) * (b - 1) * (b - 1) * (b - 1))
+	case 4:
+		numerator = int64(b * (b*b*b + 11*b*b + 11*b + 1))
+		denominator = 1
+		for i := 0; i < 5; i++ {
+			denominator *= int64(b - 1)
+		}
+	case 5:
+		numerator = int64(b * (b*b*b*b + 26*b*b*b + 66*b*b + 26*b + 1))
+		denominator = 1
+		for i := 0; i < 6; i++ {
+			denominator *= int64(b - 1)
+		}
+	case 6:
+		numerator = int64(b * (b*b*b*b*b + 57*b*b*b*b + 302*b*b*b + 302*b*b + 57*b + 1))
+		denominator = 1
+		for i := 0; i < 7; i++ {
+			denominator *= int64(b - 1)
+		}
+	case 7:
+		numerator = int64(b * (b*b*b*b*b*b + 120*b*b*b*b*b + 1191*b*b*b*b + 2416*b*b*b + 1191*b*b + 120*b + 1))
+		denominator = 1
+		for i := 0; i < 8; i++ {
+			denominator *= int64(b - 1)
+		}
+	case 8:
+		numerator = int64(b * (b*b*b*b*b*b*b + 247*b*b*b*b*b*b + 4293*b*b*b*b*b + 15619*b*b*b*b + 15619*b*b*b + 4293*b*b + 247*b + 1))
+		denominator = 1
+		for i := 0; i < 9; i++ {
+			denominator *= int64(b - 1)
+		}
+	case 9:
+		numerator = int64(b * (b*b*b*b*b*b*b*b + 502*b*b*b*b*b*b*b + 14608*b*b*b*b*b*b + 88234*b*b*b*b*b + 15619*b*b*b*b + 88234*b*b*b + 14608*b*b + 502*b + 1))
+		denominator = 1
+		for i := 0; i < 10; i++ {
+			denominator *= int64(b - 1)
+		}
+	case 10:
+		numerator = int64(b * (b*b*b*b*b*b*b*b*b + 1013*b*b*b*b*b*b*b*b + 47840*b*b*b*b*b*b*b + 455192*b*b*b*b*b*b + 1310354*b*b*b*b*b + 1310354*b*b*b*b + 455192*b*b*b + 47840*b*b + 1013*b + 1))
+		denominator = 1
+		for i := 0; i < 11; i++ {
+			denominator *= int64(b - 1)
+		}
+	}
+
+	// Сокращаем дробь
+	reduceFraction(&numerator, &denominator)
+
+	// Выводим результат в виде обыкновенной дроби
+	fmt.Print("\nРезультат: ")
+	if denominator == 1 {
+		fmt.Printf("%d/1\n", numerator)
+	} else {
+		fmt.Printf("%d/%d\n", numerator, denominator)
+	}
 }
--- series.cpp
+++ series.cpp
@@ -1,49 +1,220 @@
 #include <iostream>
+
 #include <cmath>
-#include <iomanip>
+
+#include <cstdint>
+
  
+
 using namespace std;
+
  
+
+// Функция для нахождения НОД (алгоритм Евклида)
+
+int64_t gcd(int64_t x, int64_t y) {
+
+    while (y != 0) {
+
+        int64_t temp = y;
+
+        y = x % y;
+
+        x = temp;
+
+    }
+
+    return x;
+
+}
+
+ 
+
+// Функция для сокращения дроби
+
+void reduceFraction(int64_t& numerator, int64_t& denominator) {
+
+    int64_t g = gcd(abs(numerator), abs(denominator));
+
+    numerator /= g;
+
+    denominator /= g;
+
+}
+
+ 
+
 int main() {
+
     int a, b;
-    double eps;
-    cout << "СУММА БЕСКОНЕЧНОГО РЯДА" << endl;
+
  
-    cout << "\nВведите a (целое от 1 до 10): ";
+
+    cout << "Сумма бесконечного ряда: " << endl;
+
+ 
+
+    cout << "\nВведите a: ";
+
     cin >> a;
-    cout << "Введите b (целое от 1 до 10): ";
+
+    cout << "Введите b: ";
+
     cin >> b;
-    cout << "Введите точность eps: ";
-    cin >> eps;
+
  
+
     if (a < 1 || a > 10 || b < 1 || b > 10) {
+
         cout << "Ошибка! a и b должны быть от 1 до 10" << endl;
+
         return 1;
+
     }
+
  
+
     // Если b = 1, ряд расходится
+
     if (b == 1) {
+
         cout << "\nРезультат: infinity" << endl;
+
         return 0;
+
     }
+
  
-    double sum = 0.0;
-    double currentTerm;
-    int n = 1;
+
+    int64_t numerator = 0;   // числитель
+
+    int64_t denominator = 1; // знаменатель
+
  
-    do {
-        currentTerm = pow(n, a) / pow(b, n);
-        sum += currentTerm;
-        n++;
+
+    // Вычисляем числитель и знаменатель по формулам
+
+    switch (a) {
+
+    case 1:
+
+        numerator = b;
+
+        denominator = (b - 1) * (b - 1);
+
+        break;
+
+    case 2:
+
+        numerator = b * (b + 1);
+
+        denominator = (b - 1) * (b - 1) * (b - 1);
+
+        break;
+
+    case 3:
+
+        numerator = b * (b * b + 4 * b + 1);
+
+        denominator = (b - 1) * (b - 1) * (b - 1) * (b - 1);
+
+        break;
+
+    case 4:
+
+        numerator = b * (b * b * b + 11 * b * b + 11 * b + 1);
+
+        denominator = (b - 1) * (b - 1) * (b - 1) * (b - 1) * (b - 1);
+
+        break;
+
+    case 5:
+
+        numerator = b * (b * b * b * b + 26 * b * b * b + 66 * b * b + 26 * b + 1);
+
+        denominator = 1;
+
+        for (int i = 0; i < 6; i++) denominator *= (b - 1);
+
+        break;
+
+    case 6:
+
+        numerator = b * (b * b * b * b * b + 57 * b * b * b * b + 302 * b * b * b + 302 * b * b + 57 * b + 1);
+
+        denominator = 1;
+
+        for (int i = 0; i < 7; i++) denominator *= (b - 1);
+
+        break;
+
+    case 7:
+
+        numerator = b * (b * b * b * b * b * b + 120 * b * b * b * b * b + 1191 * b * b * b * b + 2416 * b * b * b + 1191 * b * b + 120 * b + 1);
+
+        denominator = 1;
+
+        for (int i = 0; i < 8; i++) denominator *= (b - 1);
+
+        break;
+
+    case 8:
+
+        numerator = b * (b * b * b * b * b * b * b + 247 * b * b * b * b * b * b + 4293 * b * b * b * b * b + 15619 * b * b * b * b + 15619 * b * b * b + 4293 * b * b + 247 * b + 1);
+
+        denominator = 1;
+
+        for (int i = 0; i < 9; i++) denominator *= (b - 1);
+
+        break;
+
+    case 9:
+
+        numerator = b * (b * b * b * b * b * b * b * b + 502 * b * b * b * b * b * b * b + 14608 * b * b * b * b * b * b + 88234 * b * b * b * b * b + 15619 * b * b * b * b + 88234 * b * b * b + 14608 * b * b + 502 * b + 1);
+
+        denominator = 1;
+
+        for (int i = 0; i < 10; i++) denominator *= (b - 1);
+
+        break;
+
+    case 10:
+
+        numerator = b * (b * b * b * b * b * b * b * b * b + 1013 * b * b * b * b * b * b * b * b + 47840 * b * b * b * b * b * b * b + 455192 * b * b * b * b * b * b + 1310354 * b * b * b * b * b + 1310354 * b * b * b * b + 455192 * b * b * b + 47840 * b * b + 1013 * b + 1);
+
+        denominator = 1;
+
+        for (int i = 0; i < 11; i++) denominator *= (b - 1);
+
+        break;
+
+    }
+
  
-        if (n > 1000000) break;
+
+    // Сокращаем дробь
+
+    reduceFraction(numerator, denominator);
+
  
-    } while (currentTerm > eps);
+
+    // Выводим результат в виде обыкновенной дроби
+
+    cout << "\nРезультат: ";
+
+    if (denominator == 1) {
+
+        cout << numerator << "/1" << endl;
+
+    }
+
+    else {
+
+        cout << numerator << "/" << denominator << endl;
+
+    }
+
  
-    cout << fixed << setprecision(3);
-    cout << "Сумма ряда: " << sum << endl;
-    cout << "Количество итераций: " << n - 1 << endl;
- 
+
     return 0;
 }
- 
