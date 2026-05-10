7e368121ee1114d19b3c07686ce72bab61496a5a
SafronovaAnn <dolgovaannochka05@mail.ru>
2026-05-11 01:25:24 +0700

    Обновлено задание 3(Исправлена ошибка)

--- go_series.go
+++ go_series.go
@@ -1,117 +1,83 @@
 package main
 
 import (
-	"fmt"
-)
+    "fmt"
+    )
 
-// Функция для нахождения НОД (алгоритм Евклида)
-func gcd(x, y int64) int64 {
-	for y != 0 {
-		x, y = y, x%y
-	}
-	return x
+    // наибольший общий делитель
+    func gcd(x, y int64) int64 {
+    for y != 0 {
+        x, y = y, x% y
+    }
+    return x
 }
 
-// Функция для сокращения дроби
-func reduceFraction(numerator, denominator *int64) {
-	g := gcd(abs(*numerator), abs(*denominator))
-	*numerator /= g
-	*denominator /= g
+// сокращение дроби
+func reduceFraction(num, den* int64) {
+g: = gcd(*num, *den)
+* num /= g
+* den /= g
 }
 
-func abs(x int64) int64 {
-	if x < 0 {
-		return -x
-	}
-	return x
+// быстрое возведение в степень
+func power(base, exp int64) int64 {
+res: = int64(1)
+for exp > 0 {
+    if exp % 2 == 1 {
+        res *= base
+    }
+    base *= base
+        exp /= 2
+}
+return res
 }
 
 func main() {
-	var a, b int
-	fmt.Println("Сумма бесконечного ряда")
+    var a, b int64
 
-	fmt.Print("\nВведите a: ")
-	fmt.Scan(&a)
-	fmt.Print("Введите b: ")
-	fmt.Scan(&b)
+        fmt.Println("Сумма бесконечного ряда")
+        fmt.Print("Введите a: ")
+        fmt.Scan(&a)
+        fmt.Print("Введите b: ")
+        fmt.Scan(&b)
 
-	if a < 1 || a > 10 || b < 1 || b > 10 {
-		fmt.Println("Ошибка! a и b должны быть от 1 до 10")
-		return
-	}
+        if b == 1 {
+            fmt.Println("infinity")
+                return
+        }
 
-	// Если b = 1, ряд расходится
-	if b == 1 {
-		fmt.Println("\nРезультат: infinity")
-		return
-	}
+    // вектор для конечных разностей
+diff: = make([]int64, a + 2)
+for i : = int64(1); i <= a + 1; i++ {
+    diff[i] = power(i, a)
+}
 
-	var numerator int64 = 0
-	var denominator int64 = 1
+// вектор коэффициентов формулы Эйлера
+delta_A1: = make([]int64, a + 1)
+delta_A1[0] = diff[1]
 
-	// Вычисляем числитель и знаменатель по формулам
-	switch a {
-	case 1:
-		numerator = int64(b)
-		denominator = int64((b - 1) * (b - 1))
-	case 2:
-		numerator = int64(b * (b + 1))
-		denominator = int64((b - 1) * (b - 1) * (b - 1))
-	case 3:
-		numerator = int64(b * (b*b + 4*b + 1))
-		denominator = int64((b - 1) * (b - 1) * (b - 1) * (b - 1))
-	case 4:
-		numerator = int64(b * (b*b*b + 11*b*b + 11*b + 1))
-		denominator = 1
-		for i := 0; i < 5; i++ {
-			denominator *= int64(b - 1)
-		}
-	case 5:
-		numerator = int64(b * (b*b*b*b + 26*b*b*b + 66*b*b + 26*b + 1))
-		denominator = 1
-		for i := 0; i < 6; i++ {
-			denominator *= int64(b - 1)
-		}
-	case 6:
-		numerator = int64(b * (b*b*b*b*b + 57*b*b*b*b + 302*b*b*b + 302*b*b + 57*b + 1))
-		denominator = 1
-		for i := 0; i < 7; i++ {
-			denominator *= int64(b - 1)
-		}
-	case 7:
-		numerator = int64(b * (b*b*b*b*b*b + 120*b*b*b*b*b + 1191*b*b*b*b + 2416*b*b*b + 1191*b*b + 120*b + 1))
-		denominator = 1
-		for i := 0; i < 8; i++ {
-			denominator *= int64(b - 1)
-		}
-	case 8:
-		numerator = int64(b * (b*b*b*b*b*b*b + 247*b*b*b*b*b*b + 4293*b*b*b*b*b + 15619*b*b*b*b + 15619*b*b*b + 4293*b*b + 247*b + 1))
-		denominator = 1
-		for i := 0; i < 9; i++ {
-			denominator *= int64(b - 1)
-		}
-	case 9:
-		numerator = int64(b * (b*b*b*b*b*b*b*b + 502*b*b*b*b*b*b*b + 14608*b*b*b*b*b*b + 88234*b*b*b*b*b + 15619*b*b*b*b + 88234*b*b*b + 14608*b*b + 502*b + 1))
-		denominator = 1
-		for i := 0; i < 10; i++ {
-			denominator *= int64(b - 1)
-		}
-	case 10:
-		numerator = int64(b * (b*b*b*b*b*b*b*b*b + 1013*b*b*b*b*b*b*b*b + 47840*b*b*b*b*b*b*b + 455192*b*b*b*b*b*b + 1310354*b*b*b*b*b + 1310354*b*b*b*b + 455192*b*b*b + 47840*b*b + 1013*b + 1))
-		denominator = 1
-		for i := 0; i < 11; i++ {
-			denominator *= int64(b - 1)
-		}
-	}
+// вычисляем конечные разности
+for step : = int64(1); step <= a; step++ {
+    for i : = int64(1); i <= a + 1 - step; i++ {
+        diff[i] = diff[i + 1] - diff[i]
+    }
+    delta_A1[step] = diff[1]
+}
 
-	// Сокращаем дробь
-	reduceFraction(&numerator, &denominator)
+// числитель по формуле Эйлера
+var num int64 = 0
+b_minus_1 : = b - 1
 
-	// Выводим результат в виде обыкновенной дроби
-	fmt.Print("\nРезультат: ")
-	if denominator == 1 {
-		fmt.Printf("%d/1\n", numerator)
-	} else {
-		fmt.Printf("%d/%d\n", numerator, denominator)
-	}
+for k : = int64(0); k <= a; k++ {
+    num += delta_A1[k] * power(b_minus_1, a - k)
+}
+
+// знаменатель
+den: = power(b_minus_1, a + 1)
+
+// сокращаем дробь
+reduceFraction(&num, &den)
+
+// вывод
+fmt.Printf("\nРезультат: %d/%d\n", num, den)
 }
--- series.cpp
+++ series.cpp
@@ -1,220 +1,85 @@
 #include <iostream>
-
-#include <cmath>
-
+#include <vector>
 #include <cstdint>
-
- 
 
 using namespace std;
 
- 
-
-// Функция для нахождения НОД (алгоритм Евклида)
-
+// наибольший общий делитель
 int64_t gcd(int64_t x, int64_t y) {
-
     while (y != 0) {
-
-        int64_t temp = y;
-
+        int64_t t = y;
         y = x % y;
-
-        x = temp;
-
+        x = t;
     }
-
     return x;
-
 }
 
- 
-
-// Функция для сокращения дроби
-
-void reduceFraction(int64_t& numerator, int64_t& denominator) {
-
-    int64_t g = gcd(abs(numerator), abs(denominator));
-
-    numerator /= g;
-
-    denominator /= g;
-
+// сокращение дроби
+void reduceFraction(int64_t& num, int64_t& den) {
+    int64_t g = gcd(num, den);
+    num /= g;
+    den /= g;
 }
 
- 
+// быстрое возведение в степень
+int64_t power(int64_t base, int64_t exp) {
+    int64_t res = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) res *= base;
+        base *= base;
+        exp /= 2;
+    }
+    return res;
+}
 
 int main() {
+    int64_t a, b;
 
-    int a, b;
-
- 
-
-    cout << "Сумма бесконечного ряда: " << endl;
-
- 
-
-    cout << "\nВведите a: ";
-
+    cout << "Сумма бесконечного ряда\n";
+    cout << "Введите a: ";
     cin >> a;
-
     cout << "Введите b: ";
-
     cin >> b;
 
- 
-
-    if (a < 1 || a > 10 || b < 1 || b > 10) {
-
-        cout << "Ошибка! a и b должны быть от 1 до 10" << endl;
-
-        return 1;
-
-    }
-
- 
-
-    // Если b = 1, ряд расходится
-
     if (b == 1) {
-
-        cout << "\nРезультат: infinity" << endl;
-
+        cout << "infinity\n";
         return 0;
-
     }
 
- 
-
-    int64_t numerator = 0;   // числитель
-
-    int64_t denominator = 1; // знаменатель
-
- 
-
-    // Вычисляем числитель и знаменатель по формулам
-
-    switch (a) {
-
-    case 1:
-
-        numerator = b;
-
-        denominator = (b - 1) * (b - 1);
-
-        break;
-
-    case 2:
-
-        numerator = b * (b + 1);
-
-        denominator = (b - 1) * (b - 1) * (b - 1);
-
-        break;
-
-    case 3:
-
-        numerator = b * (b * b + 4 * b + 1);
-
-        denominator = (b - 1) * (b - 1) * (b - 1) * (b - 1);
-
-        break;
-
-    case 4:
-
-        numerator = b * (b * b * b + 11 * b * b + 11 * b + 1);
-
-        denominator = (b - 1) * (b - 1) * (b - 1) * (b - 1) * (b - 1);
-
-        break;
-
-    case 5:
-
-        numerator = b * (b * b * b * b + 26 * b * b * b + 66 * b * b + 26 * b + 1);
-
-        denominator = 1;
-
-        for (int i = 0; i < 6; i++) denominator *= (b - 1);
-
-        break;
-
-    case 6:
-
-        numerator = b * (b * b * b * b * b + 57 * b * b * b * b + 302 * b * b * b + 302 * b * b + 57 * b + 1);
-
-        denominator = 1;
-
-        for (int i = 0; i < 7; i++) denominator *= (b - 1);
-
-        break;
-
-    case 7:
-
-        numerator = b * (b * b * b * b * b * b + 120 * b * b * b * b * b + 1191 * b * b * b * b + 2416 * b * b * b + 1191 * b * b + 120 * b + 1);
-
-        denominator = 1;
-
-        for (int i = 0; i < 8; i++) denominator *= (b - 1);
-
-        break;
-
-    case 8:
-
-        numerator = b * (b * b * b * b * b * b * b + 247 * b * b * b * b * b * b + 4293 * b * b * b * b * b + 15619 * b * b * b * b + 15619 * b * b * b + 4293 * b * b + 247 * b + 1);
-
-        denominator = 1;
-
-        for (int i = 0; i < 9; i++) denominator *= (b - 1);
-
-        break;
-
-    case 9:
-
-        numerator = b * (b * b * b * b * b * b * b * b + 502 * b * b * b * b * b * b * b + 14608 * b * b * b * b * b * b + 88234 * b * b * b * b * b + 15619 * b * b * b * b + 88234 * b * b * b + 14608 * b * b + 502 * b + 1);
-
-        denominator = 1;
-
-        for (int i = 0; i < 10; i++) denominator *= (b - 1);
-
-        break;
-
-    case 10:
-
-        numerator = b * (b * b * b * b * b * b * b * b * b + 1013 * b * b * b * b * b * b * b * b + 47840 * b * b * b * b * b * b * b + 455192 * b * b * b * b * b * b + 1310354 * b * b * b * b * b + 1310354 * b * b * b * b + 455192 * b * b * b + 47840 * b * b + 1013 * b + 1);
-
-        denominator = 1;
-
-        for (int i = 0; i < 11; i++) denominator *= (b - 1);
-
-        break;
-
+    // вектор для конечных разностей
+    vector<int64_t> diff(a + 2);
+    for (int64_t i = 1; i <= a + 1; ++i) {
+        diff[i] = power(i, a);
     }
 
- 
+    // вектор коэффициентов формулы Эйлера
+    vector<int64_t> delta_A1(a + 1);
+    delta_A1[0] = diff[1];
 
-    // Сокращаем дробь
-
-    reduceFraction(numerator, denominator);
-
- 
-
-    // Выводим результат в виде обыкновенной дроби
-
-    cout << "\nРезультат: ";
-
-    if (denominator == 1) {
-
-        cout << numerator << "/1" << endl;
-
+    // вычисляем конечные разности
+    for (int64_t step = 1; step <= a; ++step) {
+        for (int64_t i = 1; i <= a + 1 - step; ++i) {
+            diff[i] = diff[i + 1] - diff[i];
+        }
+        delta_A1[step] = diff[1];
     }
 
-    else {
+    // числитель по формуле Эйлера
+    int64_t num = 0;
+    int64_t b_minus_1 = b - 1;
 
-        cout << numerator << "/" << denominator << endl;
-
+    for (int64_t k = 0; k <= a; ++k) {
+        num += delta_A1[k] * power(b_minus_1, a - k);
     }
 
- 
+    // знаменатель
+    int64_t den = power(b_minus_1, a + 1);
+
+    // сокращаем дробь
+    reduceFraction(num, den);
+
+    // вывод
+    cout << "\nРезультат: " << num << "/" << den << "\n";
 
     return 0;
 }
